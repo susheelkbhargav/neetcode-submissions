95b1b84c050198e251647604b7ea195b45a20077
Susheel Kumar Bhargav Kaparaboyna <susheelkbhargav@gmail.com>
2026-07-14 22:28:41 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int beg = 0;
+       int end = nums.length-1;
+       while (beg <= end){
+          int mid = (beg+end)/2;
+          if(target == nums[mid]) {
+            return mid;
+          } else if( target > nums[mid]){
+            beg=mid+1;
+          } else {
+            end = mid -1;
+          }
+       }
+       return -1;
+    }
+}
